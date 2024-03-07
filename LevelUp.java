8ab9d62016839caa60483bba9f698c50466d5a96
Renzoki <renzonifico@gmail.com>
2024-03-07 22:37:05 +0800

    Moved runeCost to class attribute, added getter

--- LevelUp.java
+++ LevelUp.java
@@ -1,11 +1,14 @@
 public class LevelUp {
 
+    //The cost for leveling up;
+    private int runeCost = 0;
+
     /**
      * Upon instantiation, the object levels up the given
      * player in the parameter based on the given attribute in the parameter.
      */
     public LevelUp(Player P, String attribute){
-        int runeCost = (P.getLevel() * 100) / 2;
+        runeCost = (P.getLevel() * 100) / 2;
         P.levelUp(runeCost);
 
         switch (attribute) {
@@ -16,5 +19,12 @@ public class LevelUp {
             case "STR" -> P.levelUpStrength();
             case "FTH" -> P.levelUpFaith();
         }
+    }
+
+    /**
+     * Method for getting the runeCost for Displaying
+     */
+    public int getRuneCost(){
+        return runeCost;
     }
 }
