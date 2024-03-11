2c61e7431c6ec26077f01350aeec68cb4a1814c0
Renzoki <renzonifico@gmail.com>
2024-03-11 23:08:53 +0800

    Now only handles atk calculation

--- Boss.java
+++ Boss.java
@@ -1,56 +1,20 @@
 import java.util.Random;
 
-public class Boss extends NPC {
+public class Boss extends Hostile_Entity {
     private final int areaIndex; //From where the Boss is from
 
     public Boss(int areaIndex){
-
+        new BossGenerator(this, areaIndex);
         this.areaIndex = areaIndex;
-
-        setName();
-        setHealth();
-        setDefense();
-        setDamage();
         this.runeDrop = health * 5;
     }
 
-    //Set Enemy Name
-    protected void setName(){
-
-        String[] BOSS_NAMES = {"Godrick the Grafted", "Rennala, Queen of the Full Moon", "The Elden Beast"};
-
-        this.name = BOSS_NAMES[areaIndex - 1];
-    }
-
-    //Set Enemy Health
-    protected void setHealth(){
-
-        int[] BOSS_HP_VALUES = {200, 400, 800};
-
-        this.health = BOSS_HP_VALUES[areaIndex - 1];
-
-    }
-
-    //Set Boss Defense
-    protected void setDefense(){
-
-        //Physical, Sorcery, and Incantation defense respectively.
-        double[][] BOSS_DEFENSE = {{0.35, 0.20, 0.15}, {0.15, 0.35, 0.25}, {0.25, 0.50, 0.40}};
-
-        this.phyDef = BOSS_DEFENSE[areaIndex - 1][0];
-        this.sorDef = BOSS_DEFENSE[areaIndex - 1][1];
-        this.incDef = BOSS_DEFENSE[areaIndex - 1][2];
-
-    }
-
     //Set Boss Damage
-    protected void setDamage(){
-
+    public int calculateDamage(){
         int[][] BOSS_ATK_VALUES ={{150, 300}, {200, 300}, {250, 500}};
 
         Random R = new Random();
-
-        this.damage = (BOSS_ATK_VALUES[areaIndex - 1][R.nextInt(2)]);
+        setDamage(BOSS_ATK_VALUES[areaIndex - 1][R.nextInt(2)]);
+        return BOSS_ATK_VALUES[areaIndex - 1][R.nextInt(2)];
     }
-
 }
