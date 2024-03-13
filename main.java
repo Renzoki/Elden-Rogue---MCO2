5f30cedd94762a89120871205d88bb20969f7fb2
Loren <136336114+debianaa15@users.noreply.github.com>
2024-03-13 16:23:00 +0800

    Add files via upload
    Title Screen

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class main{
+
+    public static void main(String[] args){
+        new TitleScreen();
+       
+    }
+}
