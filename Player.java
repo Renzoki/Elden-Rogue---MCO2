3f586296de87bfc15d225f5e418ebab7a7dc5f13
Renzoki <renzonifico@gmail.com>
2024-03-14 15:45:17 +0800

    Moved attribute lvlUp method implementation to playerManager

--- Player.java
+++ Player.java
@@ -1,8 +1,6 @@
-import java.util.List;
-
 public class Player extends Player_Entity{
 
-    PlayerManager PL = new PlayerManager();
+    PlayerManager PlayerManager = new PlayerManager();
 
     /** Constructor Method for the Player class.
      * Upon creation, a player is given a name, a job, and a job index.
@@ -11,65 +9,41 @@ public class Player extends Player_Entity{
     public Player(String name, String job, int jobIndex){
         this.playerName = name;
         this.playerJob = job;
-
-        new JobStats(this, jobIndex);//Set the player's HP, DEX, INT, END, STR, FTH
+ 
+        new JobStats(this, jobIndex - 1);//Set the player's HP, DEX, INT, END, STR, FTH
     }
 
     //Action Methods
     public void addRunes(int runes){
-        PL.addRunes(this, runes);
+        PlayerManager.addRunes(this, runes);
     }
 
     public void buyWeapon(Weapon W, int runeCost){
-        PL.buyWeapon(this, W, runeCost);
+        PlayerManager.buyWeapon(this, W, runeCost);
     }
 
     public void equipWeapon(String weaponName){
-        PL.equipWeapon(this, weaponName);
+        PlayerManager.equipWeapon(this, weaponName);
     }
 
-    public void levelUp(int runeCost){
-        PL.levelUp(this, runeCost);
-    }
-
-    public void levelUpHealth() {
-        PL.levelUpHealth(this);
-    }
-
-    public void levelUpDexterity() {
-        PL.levelUpDexterity(this);
-    }
-
-    public void levelUpIntelligence() {
-        PL.levelUpIntelligence(this);
-    }
-
-    public void levelUpEndurance() {
-        PL.levelUpEndurance(this);
-    }
-
-    public void levelUpStrength() {
-        PL.levelUpStrength(this);
-    }
-
-    public void levelUpFaith() {
-        PL.levelUpFaith(this);
+    public void levelUp(String attribute){
+        PlayerManager.levelUp(this, attribute);
     }
 
     public void setBattleHealth(){
-        PL.setBattleHealth(this);
+        PlayerManager.setBattleHealth(this);
     }
 
     public void attack(Hostile_Entity G, String atkType){
-        PL.attack(this, G, atkType);
+        PlayerManager.attack(this, G, atkType);
     }
 
     public void takeDamage(int damage){
-        PL.takeDamage(this, damage);
+        PlayerManager.takeDamage(this, damage);
     }
 
     public int getDodgeChance(){
-        return PL.getDodgeChance(this);
+        return PlayerManager.getDodgeChance(this);
     }
 }
 
